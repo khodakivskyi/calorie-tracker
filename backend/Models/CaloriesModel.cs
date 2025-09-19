7cb9b7894dbf3147fb2fb113d176552421ea251d
admin <andriyhodak2007@gmail.com>
2025-09-19 10:40:39 +0300

    improve program.cs DI, fix problems, testing graphql

---
+++ backend/Models/CaloriesModel.cs
@@ -0,0 +1,15 @@
+namespace backend.Models
+{
+    public class CaloriesModel
+    {
+        public int Id { get; }
+        public decimal Calories { get; set; }
+        public int FoodId { get; set; }
+
+        public CaloriesModel(int foodId, decimal calories)
+        {
+            FoodId = foodId;
+            Calories = calories;
+        }
+    }
+}
