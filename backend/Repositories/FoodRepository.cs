bd85934aa654ac92a86b8cd213e89a9c799957fa
Hurynchuk Sviatoslav <cviatoslav83@gmail.com>
2025-09-26 15:36:24 +0300

    repository CRUD ready, next up: FoodService

--- backend/Repositories/FoodRepository.cs
+++ backend/Repositories/FoodRepository.cs
@@ -26,5 +26,44 @@ namespace backend.Repositories
             const string sql = @"SELECT id, owner_id AS OwnerId, name, image_id AS ImageId, created_at AS CreatedAt, source, external_id AS ExternalId FROM foods WHERE owner_id = @OwnerId";
             return await connection.QueryAsync<Food>(sql, new { OwnerId = ownerId });
         }
+
+        public async Task<Food?> CreateFoodAsync(Food food)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            const string sql = @"
+                INSERT INTO foods (owner_id, name, image_id, source, external_id)
+                VALUES (@OwnerId, @Name, @ImageId, @Source, @ExternalId);
+                SELECT CAST(SCOPE_IDENTITY() as int)";
+
+            var foodId = await connection.QuerySingleAsync<int>(sql, food);
+
+            return await GetFoodByIdAsync(foodId);
+        }
+
+        public async Task<Food?> UpdateFoodAsync(Food food)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            const string sql = @"
+                UPDATE foods
+                SET name = @Name,
+                    image_id = @ImageId,
+                    source = @Source,
+                    external_id = @ExternalId
+                WHERE id = @Id";
+
+            var affectedRows = await connection.ExecuteAsync(sql, food);
+
+            if (affectedRows > 0)
+                return await GetFoodByIdAsync(food.Id);
+            else return null;
+        }
+
+        public async Task<bool> DeleteFoodAsync(int id)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            const string sql = "DELETE FROM foods WHERE id = @Id";
+            var affectedRows = await connection.ExecuteAsync(sql, new { Id = id });
+            return affectedRows > 0;
+        }
     }
 }
