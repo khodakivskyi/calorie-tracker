f26d2c1cc551d62192174a403520b91677badad1
admin <andriyhodak2007@gmail.com>
2025-10-24 16:02:41 +0300

    implement rest-controller for images

---
+++ backend/Controllers/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        public readonly ImageService _imageService;
+
+        public ImageController(ImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload([FromForm] IFormFile file, [FromForm] int userId)
+        {
+            //try
+
+            var image = await _imageService.SaveImageAsync(file, userId);
+
+            return Ok(new
+            {
+                imageId = image.Id,
+                url = image.Url,
+                fileName = image.FileName
+            });
+        }
+    }
+}
