27f2edae6ff2b43fbdfc66be2957917971a166bf
admin <andriyhodak2007@gmail.com>
2025-10-31 15:16:25 +0200

    fix mistake with endpoint in VerifyEmailPage

--- frontend/src/pages/VerifyEmailPage.tsx
+++ frontend/src/pages/VerifyEmailPage.tsx
@@ -17,7 +17,7 @@ export default function VerifyEmailPage() {
 
         const verifyEmail = async () => {
             try {
-                const response = await fetch("http://localhost:5000/graphql", {
+                const response = await fetch("http://localhost:5066/graphql", {
                     method: "POST",
                     headers: {"Content-Type": "application/json"},
                     body: JSON.stringify({
