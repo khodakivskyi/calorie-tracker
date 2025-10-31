c6abd31ceecfb06732e2ace8bc04b4015f6e328a
admin <andriyhodak2007@gmail.com>
2025-10-31 14:52:34 +0200

    add VerifyEmailPage component for email verification functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'*/
 import './App.css'
 
+//react-router-dom
+
 import RegisterPage from "./pages/RegisterPage.tsx";
+import VerifyEmailPage from "./pages/VerifyEmailPage.tsx";
 
 function App() {
+    const currentPath = window.location.pathname;
+    if (currentPath === "/verify-email") {
+        return <VerifyEmailPage />;
+    }
+
     return <RegisterPage />
 }
 
