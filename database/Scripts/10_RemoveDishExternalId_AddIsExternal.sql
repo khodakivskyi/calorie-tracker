a66c74465ec7086d5263bc9bdc9c7296a455f27c
Hurynchuk Sviatoslav <cviatoslav83@gmail.com>
2025-12-02 14:22:01 +0200

    add db refactor script

---
+++ database/Scripts/10_RemoveDishExternalId_AddIsExternal.sql
@@ -0,0 +1,27 @@
+USE calorie_tracker
+GO
+
+IF EXISTS (
+    SELECT 1
+    FROM sys.indexes
+    WHERE name = 'IX_dishes_external_id'
+      AND object_id = OBJECT_ID('dishes')
+)
+BEGIN
+DROP INDEX IX_dishes_external_id ON dishes;
+END
+GO
+
+IF COL_LENGTH('dishes', 'external_id') IS NOT NULL
+BEGIN
+ALTER TABLE dishes
+DROP COLUMN external_id;
+END
+GO
+
+IF COL_LENGTH('dishes', 'is_external') IS NULL
+BEGIN
+ALTER TABLE dishes
+    ADD is_external BIT NOT NULL CONSTRAINT DF_dishes_is_external DEFAULT 0;
+END
+GO
