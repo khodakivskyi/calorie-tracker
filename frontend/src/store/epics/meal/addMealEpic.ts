59a54d36fdb44e26a6121710e6f5615e3e135a89
admin <andriyhodak2007@gmail.com>
2025-12-17 23:21:36 +0200

    enhance and typify addMeal epic with new CreateMealInputType

--- frontend/src/store/epics/meal/addMealEpic.ts
+++ frontend/src/store/epics/meal/addMealEpic.ts
@@ -7,13 +7,8 @@ import type {RootEpicAction} from '../rootEpic.ts';
 import type {RootState} from "../../slices/rootReducer.ts";
 
 const createMealMutation = `
-    mutation CreateMeal($ownerId: Int!, $typeId: Int!, $name: String, $dishes: [CreateDishInput!]) {
-        createMeal(input: {
-            ownerId: $ownerId
-            typeId: $typeId
-            name: $name
-            dishes: $dishes
-        }) {
+    mutation CreateMeal($input: CreateMealInput!) {
+        createMeal(input: $input) {
             id
             ownerId
             typeId
@@ -49,7 +44,7 @@ export const addMealEpic: Epic<RootEpicAction, RootEpicAction, RootState> = acti
     action$.pipe(
         ofType(createMealRequest.type),
         mergeMap((action: CreateMealRequestAction) =>
-            from(graphqlRequest<CreateMealResponse>(createMealMutation, action.payload)).pipe(
+            from(graphqlRequest<CreateMealResponse>(createMealMutation, {input: action.payload})).pipe(
                 map(res => createMealSuccess({
                     ...res.createMeal,
                     createdAt: new Date(res.createMeal.createdAt).toISOString(),
